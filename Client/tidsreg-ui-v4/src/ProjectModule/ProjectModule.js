45d532ee9fc8eb29035dbbf504444fe4308d845f
PawanRegoti <regoti.pawan@gmail.com>
2018-04-21 12:08:35 +0200

    updated Timelog and ProjectOverview Page

--- Client/tidsreg-ui-v4/src/ProjectModule/ProjectModule.js
+++ Client/tidsreg-ui-v4/src/ProjectModule/ProjectModule.js
@@ -4,16 +4,55 @@ import Header, {  Footer } from '../Layout/Layout';
 
 class ProjectModule extends Component {
 
-render() {
-    return (
-            <div>
-                <Header/>
-                <div align="center">ProjectModule
-                </div>
-                <Footer />
-            </div>);
-        }
-    }
+	render() {
+		return (
+			<div>
+			<Header/>
+			<div align="center">
+			<table style={{width: '100%'}}>
+			<tr>
+			<td style={{width: '30%'}}>
+			<div>
+			<select name="ProjectDropDown" id="ProjectDropDown" className="button">
+			<option selected="selected" value="Nordea">Nordea</option>
+			<option value="Nets">Nets</option>
+
+			</select>
+
+			<div style={{margin: '20px'}}>
+			calender
+			</div>
+			</div>
+			</td>
+
+			<td style={{width: '70%'}} align="right">
+			<div align="center"> Chart Show
+			</div>
+			</td>
+			</tr>
+			<tr>
+			<td style={{width: '30%', alignContent: 'center'}}>
+			<div>
+			<input type="submit" name="CreateGrid" value="Go" id="CreateGrid" className="button" style={{width:'100px'}} />
+			</div>
+			</td>
+
+			<td style={{width: '70%', margin: '50px'}} align="right">
+			<div>
+
+			<input type="submit" name="UpdateChart" value="Go" id="UpdateChart" className="button" style={{width:'100px'}} />
+			<input type="submit" name="Export" value="Export" id="Export" className="button" />
+			</div>
+			</td>
+			</tr>
+			</table>
+			</div>
+
+			<Footer />			
+			</div>
+			);
+		}
+	}
 
 
-export default ProjectModule;
+	export default ProjectModule;
--- Client/tidsreg-ui-v4/src/TimeLogModule/TimeLogModule.js
+++ Client/tidsreg-ui-v4/src/TimeLogModule/TimeLogModule.js
@@ -4,16 +4,62 @@ import Header,{ Footer } from '../Layout/Layout';
 
 class TimeLogModule extends Component {
 
-render() {
-    return (
-            <div>
-                <Header/>
-                <div align="center">TimeLogModule
-                </div>
-                <Footer />
-            </div>);
-        }
-    }
+	render() {
+		return (
+			<div>
+			<Header/>
+			<div align="center">
+			<table style={{width: '100%'}}>
+			<tbody>
+			<tr>
+			<td style={{width: '30%'}}>
+			<ul className="centerlist">
+			<li style={{fontSize: 'x-large'}}>Add New Project</li>
+			<li>
+			<input name="NewProject" type="text" id="ContentPlaceHolder1_NewProject" className="textbox" style={{fontSize:'Large'}} /><br />
+			</li>
+			<li>
+			<input type="submit" name="AddNewProject" value="Add New Project" id="ContentPlaceHolder1_AddNewProject" className="button" style={{fontSize:'XX-Large'}} />
+			</li>
+			<li></li>
+			<li>
+			<input type="submit" name="Route" value="Route" id="Route" className="button" /><br/>
+			<input type="submit" name="Statistics" value="Statistics / Invoices" id="Statistics" className="button" /><br/>
+			</li>
+			</ul>
+			</td>
+			<td style={{width: '40%'}}>
+			<div align="center">
+			<input type="submit" name="Backward" value="&lt;" id="Backward" className="button" style={{fontSize:'XX-Large',width:'70px'}} />
+			<input type="submit" name="Forward" value=">" id="Forward" className="button" style={{fontSize:'XX-Large',width:'70px'}} />
+			</div>
+			<div align="center" style={{margin: '30px'}}>
+			<div>
+			<table className="grid" cellSpacing="4" cellPadding="8" align="Center" id="ContentPlaceHolder1_GridView1">
+			<thead>
+			<tr>
+			<th scope="col">&nbsp;</th><th scope="col">WorkDay</th><th scope="col">Nordea</th><th scope="col">Nets</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			</tr>
+			</tbody>
+			</table>
+			</div>
+			</div>
+			</td>
+			</tr>
+			</tbody>
+			</table>
+			</div>
+			<Footer />
+			</div>);
+		}
+	}
 
 
-export default TimeLogModule;
+	export default TimeLogModule;
